cf7b6781229d171830c1922d78f1ca0b30a061e9
zml <zml@stellardrift.ca>
2023-08-11 13:43:31 -0700

    fix: correct canonical rels in page URLs
    This rips the workaround from the Pallets theme, which Furo (our theme)
    is considering using
    
    Fixes #9

--- source/_ext/vineflower_site_extensions/__init__.py
+++ source/_ext/vineflower_site_extensions/__init__.py
@@ -25,12 +25,14 @@
 import re
 
 from sphinx.application import Sphinx
+from sphinx.builders.dirhtml import DirectoryHTMLBuilder
 from sphinx.roles import code_role
 from docutils.parsers.rst import nodes
 
 def setup(app: Sphinx):
     app.add_role("mojira", mojira_role)
     app.add_role("java", highlight_role("java"))
+    app.connect("html-page-context", canonical_url)
 
 
 _issue_regex = re.compile(r'[A-Z]+-[1-9][0-9]*')
@@ -58,4 +60,29 @@ def highlight_role(language: str):
         return code_role(role, rawtext, text, lineno, inliner, options=extended_options, content=content)
 
     return lang_highlight_role
+
+def canonical_url(app: Sphinx, pagename, templatename, context, doctree):
+    """Sphinx 1.8 builds a canonical URL if ``html_baseurl`` config is
+    set. However, it builds a URL ending with ".html" when using the
+    dirhtml builder, which is incorrect. Detect this and generate the
+    correct URL for each page.
+
+    Shamelessly ripped from https://github.com/pallets/pallets-sphinx-themes/blob/7a6fa47323af403c19008a5df8b5302a1ead7be3/src/pallets_sphinx_themes/__init__.py#L72C1-L91C39
+
+    Copyright 2007 Pallets `License <https://github.com/pallets/pallets-sphinx-themes/blob/7a6fa47323af403c19008a5df8b5302a1ead7be3/LICENSE.rst>`_
+    """
+    base = app.config.html_baseurl
+
+    if (
+        not base
+        or not isinstance(app.builder, DirectoryHTMLBuilder)
+        or not context["pageurl"]
+        or not context["pageurl"].endswith(".html")
+    ):
+        return
+
+    # Fix pageurl for dirhtml builder if this version of Sphinx still
+    # generates .html URLs.
+    target = app.builder.get_target_uri(pagename)
+    context["pageurl"] = base + target
 
